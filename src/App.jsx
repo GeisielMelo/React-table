3c4e5571a6d4425aaf5bda591d15714397dfc55f
Geisiel Nascimento <121590374+GeisielMelo@users.noreply.github.com>
2023-10-07 14:06:58 -0300

    feat: update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { BasicTable } from './components/BasicTable'
+import { ShortingTable } from './components/ShortingTable'
+import { FilteringTable } from './components/FilteringTable'
+import { PaginationTable } from './components/PaginationTable'
 
 function App() {
   return (
-    <BasicTable/>
+    <PaginationTable/>
   )
 }
 
