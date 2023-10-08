ad51564f62b43c8d925d4f8b6141e5f1218fbf7a
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2023-10-07 21:27:21 -0300

    feat: row selection example

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,11 @@ import { BasicTable } from './components/BasicTable'
 import { ShortingTable } from './components/ShortingTable'
 import { FilteringTable } from './components/FilteringTable'
 import { PaginationTable } from './components/PaginationTable'
+import { RowSelection } from './components/RowSelection'
 
 function App() {
   return (
-    <PaginationTable/>
+    <RowSelection/>
   )
 }
 
