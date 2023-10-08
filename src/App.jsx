8b40e5155e192e437d2f4e84e320c7bcaae313a2
Geisiel Nascimento <geisiel.nascimento@gmail.com>
2023-10-08 14:28:23 -0300

    feat: created a Table.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import React from 'react'
-import { BasicTable } from './components/BasicTable'
-import { ShortingTable } from './components/ShortingTable'
-import { FilteringTable } from './components/FilteringTable'
-import { PaginationTable } from './components/PaginationTable'
-import { RowSelection } from './components/RowSelection'
+import React from "react";
+import { Table } from "./components/Table";
 
 function App() {
   return (
-    <PaginationTable/>
-  )
+    <div>
+      <Table />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Table.jsx
@@ -0,0 +1,153 @@
+import React, { useMemo } from "react";
+import {
+  useTable,
+  usePagination,
+  useRowSelect,
+  useGlobalFilter,
+  useSortBy,
+} from "react-table";
+import MOCK_DATA from "./MOCK_DATA.json";
+import { COLUMNS } from "./columns";
+import { Checkbox } from "./Checkbox";
+import { GlobalFilter } from "./GlobalFilter";
+import "./table.css";
+
+export const Table = () => {
+  const columns = useMemo(() => COLUMNS, []);
+  const data = useMemo(() => MOCK_DATA, []);
+
+  const tableInstance = useTable(
+    {
+      columns,
+      data,
+    },
+    useGlobalFilter,
+    useSortBy,
+    usePagination,
+    useRowSelect,
+    (hooks) => {
+      hooks.visibleColumns.push((columns) => {
+        return [
+          {
+            id: "selection",
+            Header: ({ getToggleAllRowsSelectedProps }) => (
+              <Checkbox {...getToggleAllRowsSelectedProps()} />
+            ),
+            Cell: ({ row }) => (
+              <Checkbox {...row.getToggleRowSelectedProps()} />
+            ),
+          },
+          ...columns,
+        ];
+      });
+    }
+  );
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    page,
+    nextPage,
+    previousPage,
+    canNextPage,
+    canPreviousPage,
+    pageOptions,
+    setPageSize,
+    state,
+    pageCount,
+    gotoPage,
+    prepareRow,
+    selectedFlatRows,
+    setGlobalFilter,
+  } = tableInstance;
+
+  const { globalFilter } = state;
+  const { pageIndex, pageSize } = state;
+
+  return (
+    <>
+      <GlobalFilter filter={globalFilter} setFilter={setGlobalFilter} />
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                  {column.render("Header")}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+
+        <tbody {...getTableBodyProps()}>
+          {page.map((row) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <div>
+        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+          {"<<"}
+        </button>
+        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+          {"<"}
+        </button>
+
+        <span>
+          Page {pageIndex + 1} of {pageOptions.length}{" "}
+        </span>
+
+        <span>
+          Go to page:{" "}
+          <input
+            type="number"
+            placeholder=""
+            defaultValue={pageIndex + 1}
+            onChange={(e) => {
+              const page = e.target.value ? Number(e.target.value) - 1 : 0;
+              gotoPage(page);
+            }}
+          />
+        </span>
+
+        <button onClick={() => nextPage()} disabled={!canNextPage}>
+          {">"}
+        </button>
+        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+          {">>"}
+        </button>
+
+        <select
+          value={pageSize}
+          onChange={(e) => setPageSize(Number(e.target.value))}
+        >
+          {[10, 20, 30, 40, 50].map((pageSize) => (
+            <option key={pageSize} value={pageSize}>
+              Show {pageSize}
+            </option>
+          ))}
+        </select>
+      </div>
+      <code>
+        {JSON.stringify(
+          {
+            selectedFlatRows: selectedFlatRows.map((row) => row.original),
+          },
+          null,
+          2
+        )}
+      </code>
+    </>
+  );
+};
